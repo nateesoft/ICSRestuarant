cfcc8a17a14d1c352e71daf34ab08fc462149427
nateelive <natee.live@gmail.com>
2026-05-28 12:07:53 +0700

    update first pos application

---
+++ src/main/java/com/ics/pos/restaurant/icsrestaurant/ICSRestaurant.java
@@ -0,0 +1,12 @@
+package com.ics.pos.restaurant.icsrestaurant;
+
+/**
+ *
+ * @author nateelive
+ */
+public class ICSRestaurant {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
